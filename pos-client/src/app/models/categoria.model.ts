bbb778849bd86c4d5a9510142294628c2d3b3e5f
Cristhian Camacho <cristhiancamacho61@gmail.com>
2024-07-19 18:43:04 -0700

    Auto-generated commit

--- pos-client/src/app/models/categoria.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Categoria {
-    _id?: string;
-    name: string;
-    description?: string;
-    productos?: string[];  // Assuming it references Producto by IDs
-    usuario: string;       // Assuming it references Usuario by ID
-  }
-  
--- pos-client/src/app/models/proveedor.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Proveedor {
-    _id?: string;
-    name: string;
-    contact_info?: string;
-    products?: string[];  // Assuming it references Producto by IDs
-  }
-  
